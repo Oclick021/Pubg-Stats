1013cc72b054a9106e7ff5d26e56a179ab1e1ad8
Saeed Asgari <oclick021@gmail.com>
2019-09-22 19:24:01 +0200

    Add PreCondition For admin

---
+++ PubgStatsBot/Services/CustomPreConditions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using PubgStatsBot.Helpers;
+
+// Inherit from PreconditionAttribute
+public class IsOwner : PreconditionAttribute
+{
+
+    // Override the CheckPermissions method
+    public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+    {
+        // If this command was executed by a user with the appropriate role, return a success
+        if (context.User.Id == Credentials.UserID)
+            // Since no async work is done, the result has to be wrapped with `Task.FromResult` to avoid compiler errors
+            return Task.FromResult(PreconditionResult.FromSuccess());
+        // Since it wasn't, fail
+        else
+            return Task.FromResult(PreconditionResult.FromError($"Only Owner can use this command"));
+
+    }
+}
